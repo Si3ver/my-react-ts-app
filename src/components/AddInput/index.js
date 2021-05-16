2e87550505fcb953634ae2b3d97dc12e664e6ba0
Si3ver <izhouwl@163.com>
2021-05-16 22:40:05 +0800

    feat: 使用useRef获取input元素内容

--- src/components/AddInput/index.js
+++ src/components/AddInput/index.js
@@ -1,8 +1,21 @@
+import { useRef } from "react"
 import './index.scss'
 
 function AddInput (props) {
-  
+
   const { isInputShow } = props
+  const inputRef = useRef()
+
+  const submitValue = () => {
+    const inputValue = inputRef.current.value
+    console.log(inputRef.current, inputValue)
+
+    if (inputValue.length === 0) {
+      return
+    }
+
+    inputRef.current.value = ''
+  }
 
   return (
     <>
@@ -12,9 +25,12 @@ function AddInput (props) {
         (
           <div className="input-wrapper">
             <input type="text"
+              ref={inputRef}
               placeholder="请输入待办事件"
             />
-            <button className="btn btn-primary">
+            <button className="btn btn-primary"
+              onClick={submitValue}
+            >
               增加
             </button>
           </div>

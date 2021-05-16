e04c5dcea8b664052dae9edc740aaa512d1adc8e
Si3ver <izhouwl@163.com>
2021-05-16 23:41:29 +0800

    feat: todoItem数据展示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useCallback, useState } from 'react';
 import MyHeader from './components/Header';
 import AddInput from './components/AddInput';
-import './App.css';
+import TodoItem from './components/TodoItem';
+import './App.scss';
 
 interface todoItemProps {
   id: number;
@@ -34,6 +35,24 @@ function App() {
         isInputShow={ isInputShow }
         addItem={addItem}
       />
+      {!todoList || todoList.length === 0 ? (
+        'nodata'
+      ) : (
+        <ul className="todo-list">
+          {todoList.map((item, index) => {
+            return (
+              <TodoItem
+                dataItem={item}
+                key={index}
+                removeItem={() => {}}
+                openCheckModal={() => {}}
+                completeItem={() => {}}
+                openEditModal={() => {}}
+              />
+            )
+          })}
+        </ul>
+      )}
     </div>
   );
 }

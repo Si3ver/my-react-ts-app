2b1560cb72f46dac895fb4eff0e72ae6573453ef
Si3ver <izhouwl@163.com>
2021-05-16 18:08:37 +0800

    feat: 完成头部 和 输入框

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import MyHeader from './components/Header';
+import AddInput from './components/AddInput';
 import './App.css';
 
 function App() {
+
+  const [isInputShow, setInputShow] = useState(false)
+
   return (
     <div className="App">
-
+      <MyHeader openInput={()=>{
+        setInputShow(!isInputShow)
+      }} />
+      <AddInput
+        isInputShow={ isInputShow }
+      />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/css/resets.css'
+import './assets/css/border.css'
+import './assets/css/ui.css'
+import './assets/js/common.js'
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
